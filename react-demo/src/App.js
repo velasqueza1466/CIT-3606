2bdc97030122c7d8b7f5a2eb27b098444188ba4f
velasqueza1466 <velasqueza1466@student.elms.edu>
2025-11-03 12:07:40 -0500

    Coded
    Worked on code

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Pokemon(props) 
-  return 
-  {}
+function MyButton(props) {
+  return <button class= "button" onClick = {handler}>{props.text}</button>;
+}
+function handler() {
+  alert("Button clicked!");
+}
+
+function Pokemon(props) {
+  return (
+    <div className="pokemon-card">
+      <h2>{props.name}</h2>
+      <img src={props.image} alt={props.name} width="500" height="500"/>
+    </div>
+  ); 
+  
+}
 
 function App() {
   return (
@@ -12,8 +25,14 @@ function App() {
         <h1>
         <Pokemon name="Pikachu" image="pikachu.jpg"/>
         <Pokemon name="Snorlax" image="snorlax.jpg"/>
-        <Pokemon name="Charmander" image="charmander.wedp"/>
+        <Pokemon name="Charmander" image="charmander.webp"/>
         <Pokemon name="Dittobase" image="dittobase.png"/>
+        </h1>
+        <h1>
+          <pokemon name="Pikachu" image="pikachu.jpg"></pokemon>
+          <pokemon name="Snorlax" image="snorlax.jpg"></pokemon>
+          <pokemon name="Charmander" image="charmander.webp"></pokemon>
+          <pokemon name="Dittobase" image="dittobase.png"></pokemon>
         </h1>
       </header>
     </div>
